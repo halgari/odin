8c6617fbfe936c4c7f3515c7e1adb4af6e24f913
Timothy Baldridge <tbaldridge@gmail.com>
2016-12-10 06:38:17 -0700

    remove some boilerplate from the project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.tbaldridge/odin "0.2.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A declarative query DSL for Clojure"
+  :url "http://github.com/halgari/odin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
